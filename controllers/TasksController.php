b66df506bc62efe5b2f79eef3ef834d8d0f8f8ef
Anatolev Maksim <anatolev.web@gmail.com>
2021-12-25 15:07:32 +1000

    Update TasksController.php

--- controllers/TasksController.php
+++ controllers/TasksController.php
@@ -2,10 +2,11 @@
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
+use app\models\forms\SearchForm;
 use app\services\TaskService;
 use app\services\CategoryService;
-use app\models\forms\SearchForm;
 
 class TasksController extends Controller
 {
@@ -13,8 +14,8 @@ class TasksController extends Controller
     {
         $model = new SearchForm();
 
-        if (\Yii::$app->request->getIsPost()) {
-            $model->load(\Yii::$app->request->post());
+        if (Yii::$app->request->getIsPost()) {
+            $model->load(Yii::$app->request->post());
         }
 
         $tasks = (new TaskService())->getFilteredTasks($model);
